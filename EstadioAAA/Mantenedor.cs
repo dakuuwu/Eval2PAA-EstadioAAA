a236192c94b07124f97751d51ec36ac679469076
Luna Guerra <dakuuwu@gmail.com>
2021-05-20 23:10:59 -0400

    Última versión antes de la entrega: 20/05/2021

--- EstadioAAA/Mantenedor.cs
+++ EstadioAAA/Mantenedor.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace EstadioAAA
 {
@@ -25,18 +24,21 @@ namespace EstadioAAA
 
         public void AddUser(List<Usuario> usrlist)
         {
+            Console.WriteLine();
             Usuario usr = new Usuario();
-            System.Console.WriteLine("Por favor, ingrese el nombre del cliente:");
-            usr.Username = System.Console.ReadLine();
-            System.Console.WriteLine("Por favor, ingrese la contraseña del cliente:");
-            usr.Password = System.Console.ReadLine();
-            System.Console.WriteLine("Por favor, ingrese el e-mail del cliente:");
-            usr.Email = System.Console.ReadLine();
-            System.Console.WriteLine("Por favor, ingrese el RUT del cliente:");
-            usr.RUT = System.Console.ReadLine();
-            usr.UUID = UUIDGenerator(8);
+            Console.WriteLine("Por favor, ingrese el nombre del cliente:");
+            usr.Username = Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese la contraseña del cliente:");
+            usr.Password = Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese el e-mail del cliente:");
+            usr.Email = Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese el RUT del cliente:");
+            usr.RUT = Console.ReadLine();
+            usr.UUID = UUIDGenerator(32);
             usrlist.Add(usr);
-            System.Console.WriteLine($"El usuario con el RUT: {usr.RUT} ha sido añadido con exito. UUID: {usr.UUID}");
+            Console.WriteLine();
+            Console.WriteLine($"El usuario con el RUT: {usr.RUT} ha sido añadido con exito. UUID: {usr.UUID}");
+            Console.WriteLine();
         }
 
         public Usuario BuscarUsuario(List<Usuario> usrlist, string RUT)
@@ -44,7 +46,7 @@ namespace EstadioAAA
             Usuario usr = new Usuario();
             try
             {   
-                usr = usrlist.Single(s => s.Equals(RUT));
+                usr = usrlist.Single(s => s.RUT.Equals(RUT));
             }
             catch (InvalidOperationException e)
             {
@@ -55,36 +57,41 @@ namespace EstadioAAA
 
         public void EliminarUsuario(List<Usuario> usrlist)
         {
-            System.Console.WriteLine("Por favor, ingrese el RUT del usuario a eliminar:");
-            string RUT = System.Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Por favor, ingrese el RUT del usuario a eliminar:");
+            string RUT = Console.ReadLine();
             Usuario usr = BuscarUsuario(usrlist, RUT);
             usrlist.Remove(usr);
-            System.Console.WriteLine($"El usuario con el RUT: {RUT} y el UUID: {usr.UUID} ha sido eliminado con exito.");
+            Console.WriteLine($"El usuario con el RUT: {RUT} y el UUID: {usr.UUID} ha sido eliminado con exito.");
+            Console.WriteLine();
         }
 
         public void AddEvento(List<Evento> eventlist, List<Usuario> usrlist)
         {
+            Console.WriteLine();
             Evento evt = new Evento();
-            System.Console.WriteLine("Por favor, ingrese el nombre del evento:");
-            evt.NombreEvento = System.Console.ReadLine();
-            System.Console.WriteLine("Por favor, ingrese la descripcion del evento:");
-            evt.DescEvento = System.Console.ReadLine();
-            System.Console.WriteLine("Por favor, ingrese el valor (en pesos) del evento:");
+            Console.WriteLine("Por favor, ingrese el nombre del evento:");
+            evt.NombreEvento = Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese la descripcion del evento:");
+            evt.DescEvento = Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese el valor (en pesos) del evento:");
             int valor;
-            Int32.TryParse(System.Console.ReadLine(), out valor);
+            Int32.TryParse(Console.ReadLine(), out valor);
             evt.ValorEvento = valor;
-            System.Console.WriteLine("Por favor, ingrese el quorum maximo del evento:");
+            Console.WriteLine("Por favor, ingrese el quorum maximo del evento:");
             int quorum;
-            Int32.TryParse(System.Console.ReadLine(), out quorum);
+            Int32.TryParse(Console.ReadLine(), out quorum);
             evt.QuorumEvento = quorum;
-            System.Console.WriteLine("Por favor, ingrese el RUT del organizador:");
-            string RUT = System.Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese el RUT del organizador:");
+            string RUT = Console.ReadLine();
             Usuario usr = BuscarUsuario(usrlist, RUT);
             evt.Organizador = usr.Username;
             evt.UUID = usr.UUID;
-            evt.CodigoEvento = UUIDGenerator(8);
+            evt.CodigoEvento = UUIDGenerator(32);
             eventlist.Add(evt);
-            System.Console.WriteLine($"El evento {evt.NombreEvento} ha sido agregado con exito.");
+            Console.WriteLine();
+            Console.WriteLine($"El evento \"{evt.NombreEvento}\" ha sido agregado con exito.");
+            Console.WriteLine();
         }
 
         public Evento BuscarEvento(List<Evento> eventlist, string nomEvento)
@@ -92,7 +99,7 @@ namespace EstadioAAA
             Evento evt = new Evento();
             try
             {
-                evt = eventlist.Single(s => s.Equals(nomEvento));
+                evt = eventlist.Single(s => s.NombreEvento.Equals(nomEvento));
             }
             catch (InvalidOperationException e)
             {
@@ -103,12 +110,74 @@ namespace EstadioAAA
 
         public void EliminarEvento(List<Evento> eventlist)
         {
-            System.Console.WriteLine("Por favor, ingrese el nombre del evento a eliminar:");
-            string nomEvento = System.Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Por favor, ingrese el nombre del evento a eliminar:");
+            string nomEvento = Console.ReadLine();
             Evento evt = BuscarEvento(eventlist, nomEvento);
             eventlist.Remove(evt);
-            System.Console.WriteLine($"El usuario con el RUT: {nomEvento} ha sido eliminado con exito.");
+            Console.WriteLine($"El evento con el nombre: {nomEvento} ha sido eliminado con exito.");
+            Console.WriteLine();
         }
 
+        public void ListarUsuario(List<Usuario> usrlist)
+        {
+            foreach (Usuario a in usrlist)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Nombre: {a.Username}");
+                Console.WriteLine($"E-mail: {a.Email}");
+                Console.WriteLine($"RUT: {a.RUT}");
+                Console.WriteLine($"UUID: {a.UUID}");
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+        public void ListarEvento(List<Evento> evtlist)
+        {
+            foreach (Evento a in evtlist)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Nombre: {a.NombreEvento}");
+                Console.WriteLine($"Organizador(a): {a.Organizador}");
+                Console.WriteLine($"Valor: ${a.ValorEvento}");
+                Console.WriteLine($"Quorum: {a.QuorumEvento} Personas");
+                Console.WriteLine($"UUID Evento: {a.CodigoEvento}");
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+
+        public static int ValidateInt()
+        {
+            int num = 0;
+            while (num == 0)
+            {
+                try
+                {
+                    num = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            return num;
+        }
+        //metodo temporal para inicializar campos de datos
+        public void InicializarCampos(List<Usuario> usrlist, List<Evento> evtlist)
+        {
+            string[] usernames = {"Alex", "Alexander", "Cristian", "Bastian", "Luna", "Joaquin", "Diego", "Benjamin", "Alfredo", "Sebastian", "Artemisa", "Selene", "Nyx", "Javiera", "Selma"};
+            string[] rut = { "69696969-1", "69696969-2", "69696969-3", "69696969-4", "19363100-2", "69696969-5", "69696969-6", "69696969-7", "69696969-8", "69696969-9", "69696969-0", "69696969-K", "69696969-10", "69696969-11", "69696969-12" };
+            for (int i=0; i<usernames.Length-1; i++)
+            {
+                Usuario usr = new Usuario();
+                usr.Username = usernames[i];
+                usr.Password = UUIDGenerator(16);
+                usr.Email = $"{UUIDGenerator(12)}@mail.cl";
+                usr.RUT = rut[i];
+                usr.UUID = UUIDGenerator(32);
+                usrlist.Add(usr);
+            }
+        }
     }
 }
--- EstadioAAA/Program.cs
+++ EstadioAAA/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace EstadioAAA
 {
@@ -11,10 +10,97 @@ namespace EstadioAAA
             //variables & instances
             List<Usuario> userlist = new List<Usuario>();
             List<Evento> eventlist = new List<Evento>();
+            Mantenedor man = new Mantenedor();
+            man.InicializarCampos(userlist, eventlist);
+            int option;
+            bool loop = true;
             Console.WriteLine("Bienvenid@ al programa de Estadio AAA!");
-            Console.WriteLine("");
 
-            while()
+            while (loop)
+            {
+                Console.WriteLine("Por favor, seleccione una de las siguientes opciones:");
+                Console.WriteLine("(1) Agregar cliente");
+                Console.WriteLine("(2) Agregar evento");
+                Console.WriteLine("(3) Eliminar cliente");
+                Console.WriteLine("(4) Eliminar evento");
+                Console.WriteLine("(5) Listar clientes");
+                Console.WriteLine("(6) Listar eventos");
+                Console.WriteLine("(7) Salir");
+                option = Mantenedor.ValidateInt();
+                switch (option)
+                {
+                    case 1:
+                        try
+                        {
+                            man.AddUser(userlist);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            man.AddEvento(eventlist, userlist);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            man.EliminarUsuario(userlist);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                    case 4:
+                        try
+                        {
+                            man.EliminarEvento(eventlist);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                    case 5:
+                        try
+                        {
+                            man.ListarUsuario(userlist);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                    case 6:
+                        try
+                        {
+                            man.ListarEvento(eventlist);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+                    case 7:
+                        loop = false;
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Por favor, utilice un numero entre 1 y 7.");
+                        Console.WriteLine();
+                        System.Threading.Thread.Sleep(150);
+                        break;
+                }
+            }
         }
     }
 }
